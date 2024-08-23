aa7066d4915ab0242ed59208eb7a7a5d38765090
Koushikpuppala <koushikpuppala203@gmail.com>
2024-08-23 10:32:35 +0530

    Add next.js as a dependency in types package.json
    Signed-off-by: Koushikpuppala <koushikpuppala203@gmail.com>

--- projects/frontend/next.config.mjs
+++ projects/frontend/next.config.mjs
@@ -4,10 +4,6 @@ import path from 'path'
 const config = {
 	reactStrictMode: true,
 	output: process.env.ENVIRONMENT === 'production' ? 'standalone' : undefined,
-	experimental:
-		process.env.ENVIRONMENT === 'production'
-			? { outputFileTracingRoot: path.join(__dirname, '../../') }
-			: undefined,
 	transpilePackages: ['@repo/database', '@repo/types'],
 	images: {
 		remotePatterns: [{ protocol: 'https', hostname: 'lh3.googleusercontent.com', port: '', pathname: '**' }],
